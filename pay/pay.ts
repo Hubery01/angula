3a719e597be7160caaf81db758f7a5a964359e62
Hubery01 <49144432+Hubery01@users.noreply.github.com>
2019-04-15 09:26:52 +0800

    Add files via upload

--- pay/pay.ts
+++ pay/pay.ts
@@ -13,11 +13,11 @@ import {LoadingController, ViewController, IonicPage, NavController, NavParams }
   selector: 'page-pay',
   templateUrl: 'pay.html',
 })
-export class PayPage {
+export class PayPage { 
 
   constructor(
-    private myL:LoadingController,
-    private myV:ViewController,
+    private loadingCtrl:LoadingController,
+    private viewCtrl:ViewController,
     public navCtrl: NavController, 
     public navParams: NavParams) {
   }
@@ -27,17 +27,17 @@ export class PayPage {
   }
 
   close(){
-    this.myV.dismiss(0)
+    this.viewCtrl.dismiss(0)
   }
 
   paying(){
-    var myl = this.myL.create({
+    var myLoading = this.loadingCtrl.create({
       content:'支付中'
     })
-    myl.present()
-    console.log(myl)
+    myLoading.present()
     setTimeout(()=>{
-      this.myV.dismiss(1)
+      myLoading.dismiss()
+      this.viewCtrl.dismiss(1)
     },3000)
   }
 }
